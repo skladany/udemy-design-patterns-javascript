81123f2d6b14ba15a3efced52ae63e20d4f10acc
Steve Skladany <steve@skladany.net>
2022-05-07 12:42:58 -0400

    docs: section 2 complete

--- code/02/solid.js
+++ code/02/solid.js
@@ -20,6 +20,66 @@ class Product {
   }
 }
 
-let apple = new Product("Apple", Color.green, Size.small);
-let tree = new Product("Tree", Color.green, Size.large);
-let house = new Product("House", Color.blue, size.large);
+const apple = new Product("Apple", Color.green, Size.small);
+const tree = new Product("Tree", Color.green, Size.large);
+const house = new Product("House", Color.blue, Size.large);
+
+const products = [apple, tree, house];
+
+// General Interface for a Spec
+class ColorSpecification {
+  constructor(color) {
+    this.color = color;
+  }
+
+  isSatisfied(item) {
+    return item.color === this.color;
+  }
+}
+
+class SizeSpecification {
+  constructor(size) {
+    this.size = size;
+  }
+
+  isSatisfied(item) {
+    return item.size === this.size;
+  }
+}
+
+class BetterFilter {
+  filter(items, spec) {
+    return items.filter((x) => spec.isSatisfied(x));
+  }
+}
+
+// Specification combinator
+class AndSpecification {
+  constructor(...specs) {
+    this.specs = specs;
+  }
+
+  isSatisfied(item) {
+    return this.specs.every((x) => x.isSatisfied(item));
+  }
+}
+
+const bf = new BetterFilter();
+console.log(`Green products (new):`);
+for (const p of bf.filter(products, new ColorSpecification(Color.green))) {
+  console.log(` * ${p.name} is green`);
+}
+
+console.log(`Large products (new):`);
+for (const p of bf.filter(products, new SizeSpecification(Size.large))) {
+  console.log(` * ${p.name} is large`);
+}
+
+console.log(`Large and green products:`);
+let spec = new AndSpecification(
+  new ColorSpecification(Color.green),
+  new SizeSpecification(Size.large)
+);
+for (let p of bf.filter(products, spec)) {
+  console.log(` * ${p.name} is green and large`);
+}
--- readme.md
+++ readme.md
@@ -52,7 +52,27 @@ Notes and a checklist as I go through the course.
   - Antipattern: the god object
   - Seperation of concerns
 
-- [x] 4. Open-Closed Principle
+- [x] 5. Open-Closed Principle
 
   - Objects are open for extension (e.g, inheritance), but close for modification (e.g, adding methods, etc)
   - Instead, use specification pattern & combinator pattern
+
+- [x] 6. Liskov Substitution Principle
+
+  - A function that takes a base type, should also be able to take a derived type
+
+- [x] 7. Interface Segregation Principle
+
+  - Segregate interface into different parts so you don't need to implement things you don't need
+  - Principle of least surprise; Should be obvious
+  - In JavaScript, it's somewhat moot given the lack of interfaces in classes (due to lack of abstract classes)
+  - YAGNI - You Ain't Going to Need It
+
+- [x] 8. Depdendency Inversion Principle
+
+  - Defines a relation you should have between high-level & low-level modules
+  - Low-level module ("close to the metal", concerned with storage)
+  - High-level module (concerned with getting the data out, using it somehow)
+  - High-level modules should _not_ depending directly on low-level modules but instead depend on abtractions
+
+- [x] 9. Summary
